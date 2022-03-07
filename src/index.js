b01ca91b217b19b0192273ce5857d471fe9e057f
bea <chamoischamois@protonmail.com>
2022-03-07 21:45:31 +0800

    Re-organized some functions and added images to pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {mainPage} from './mainpage.js';
-import {menuPage} from './menupage.js';
+import {menuPage, addMenuItem} from './menupage.js';
 import {contactPage} from './contactpage.js';
 import './style.css';
-
+import homeImage from './images/amphora.png';
+import menuImage from './images/amphora(1).png';
+import contactImage from './images/vase.png';
 
 const createSideBar = (() => {
     const sideBar = document.createElement('div');
@@ -25,10 +27,38 @@ function navCreator(name, func) {
     createSideBar.navigation.appendChild(navLink);
 }
 
-navCreator('Home', mainPage);
-navCreator('Menu', menuPage);
-navCreator('Visit Us', contactPage);
+function imageInserter(pageID, image){
+    const page = document.getElementById(pageID);
+    const imageCreator = document.createElement('img');
+    imageCreator.src = image;
+    imageCreator.style.height = '100px';
+    imageCreator.style.width  = '100px';
+    imageCreator.style.display = 'block';
+    imageCreator.style.margin = '0 auto';
+    imageCreator.style.padding = '3rem';
+    page.appendChild(imageCreator);
+}
+
+navCreator('Home', () => {
+        mainPage();
+        imageInserter('main-page', homeImage);
+    }
+);
+navCreator('Menu', () => {
+        menuPage();
+        addMenuItem();
+        imageInserter('menu-page', menuImage);
+    }
+);
+navCreator('Visit Us', () => {
+        contactPage();
+        imageInserter('contact-page', contactImage);
+    }
+);
+
+
 mainPage();
+imageInserter('main-page', homeImage);
 
 
 
--- src/menupage.js
+++ src/menupage.js
@@ -7,17 +7,13 @@ function menuPage() {
 
     const cardContainer = document.createElement('div');
     menuContent.appendChild(cardContainer);
-
+    cardContainer.id = 'card-container';
     cardContainer.style.display = 'grid';
     cardContainer.style.gridTemplateColumns = '1fr 1fr';
     cardContainer.style.gap = '2rem';
-    cardContainer.style.overflow = 'auto';
 
     document.getElementById('content').appendChild(menuContent);
     
-    cardContainer.appendChild(menuItem('Floral Tea', `Our award-winning floral teas are brewed with sustainably-sourced flowers in support of our local flower farmers. The flowers are picked fresh daily, and every day a unique flavor is offered. Please approach our counter staff for the flowers of the day.`));
-    cardContainer.appendChild(menuItem('Floral Coffee', 'Introduced by popular demand, msqt now offers a coffee counterpart to the house-favorite floral tea. Available hot or iced, the floral coffee is sure to be an effective pick-me-up to start off your day. Flowers used vary daily.'));
-    cardContainer.appendChild(menuItem('Cheesecake', 'Patrons to the cafe enjoy the harmony of the smoky, crumbly biscuit base and the creamy cheese. Whole cakes have to be ordered at least one day in advance.'));
 }
 
 const menuItem = (name, desc) => {
@@ -35,8 +31,16 @@ const menuItem = (name, desc) => {
     return menuCard;
 }
 
+function addMenuItem() {
+    const cardContainer = document.getElementById('card-container');
+    
+    cardContainer.appendChild(menuItem('Floral Tea', `Our award-winning floral teas are brewed with sustainably-sourced flowers in support of our local flower farmers. The flowers are picked fresh daily, and every day a unique flavor is offered. Please approach our counter staff for the flowers of the day.`));
+    cardContainer.appendChild(menuItem('Floral Coffee', 'Introduced by popular demand, msqt now offers a coffee counterpart to the house-favorite floral tea. Available hot or iced, the floral coffee is sure to be an effective pick-me-up to start off your day. Flowers used vary daily.'));
+    cardContainer.appendChild(menuItem('Cheesecake', 'Patrons to the cafe enjoy the harmony of the smoky, crumbly biscuit base and the creamy cheese. Whole cakes have to be ordered at least one day in advance.'));
+    cardContainer.appendChild(menuItem('Sachertorte', 'Experience our take on this famous chocolate sponge cake! This rich treat is ideal for special occasions.'));
+    cardContainer.appendChild(menuItem('Croissant Sandwich', 'A filling croissant sandwich made with fresh ingredients. Vegeterian and vegan alternatives available.'));
+}
 
 
 
-
-export {menuPage};
+export {menuPage, addMenuItem};
